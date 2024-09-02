9d55cfd60177f82e9243a6bcb41afca26661e80d
Reinald Mendes <112445979+ReinaldMendes@users.noreply.github.com>
2024-09-02 09:50:06 -0300

    Update nome_maiusculo.js

--- src/middlewares/nome_maiusculo.js
+++ src/middlewares/nome_maiusculo.js
@@ -1,4 +1,14 @@
 const axios = require("axios");
+
+// Middleware para alterar o nome para maiúsculo
+const uppercaseName = (req, res, next) => {
+  if (req.body.nome) {
+    req.body.nome = req.body.nome.toUpperCase();
+  }
+  next();
+};
+
+// Middleware para buscar o endereço pelo CEP
 const cep_endereco = (req, res, next) => {
   axios
     .get(`https://viacep.com.br/ws/${req.body.endereco}/json/`)
@@ -6,7 +16,10 @@ const cep_endereco = (req, res, next) => {
       delete req.body.cep;
       req.body.endereco = resposta.data;
       next();
+    })
+    .catch((error) => {
+      res.status(500).send("Erro ao buscar endereço.");
     });
 };
 
-module.exports = cep_endereco;
+module.exports = { cep_endereco, uppercaseName };
